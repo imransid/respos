ec8121547ff4e96106ead64135c13844420c8d08
Aurnab Paul <aurnab06@gmail.com>
2024-06-02 10:39:12 +0600

    Revert "dependencies added"
    This reverts commit 6d70a1bca1c36c8d97704150f652c94bdfb52fe9.

--- App.tsx
+++ App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import HomeScreen from './src/app/Screens/HomeScreen/HomeScreen';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import HomeHeader from './src/app/Screens/HomeScreen/HomeHeader';
+import HomeScreen from './src/app/Screens/HomeScreen/HomeScreen';
+import { View } from 'react-native';
 
 const Stack = createNativeStackNavigator();
-const Drawer = createDrawerNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Ticket" component={HomeHeader} />
-      </Stack.Navigator>
-      {/* <Drawer.Navigator>
-        <Drawer.Screen name="Feed" component={HomeScreen} />
-      </Drawer.Navigator> */}
-    </NavigationContainer>
-    //<HomeHeader/>
+    // <NavigationContainer>
+    //   <Stack.Navigator>
+    //     <Stack.Screen name="Ticket" component={HomeHeader} />
+    //   </Stack.Navigator>
+    // </NavigationContainer>
+    <View>
+      <HomeHeader/>
+      <HomeScreen/>
+    </View>
+    
   );
 }
 
