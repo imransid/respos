3c34257da1d1fd43925350434009ae189258a268
Aurnab Paul <aurnab06@gmail.com>
2024-06-02 16:07:14 +0600

    Icons are added but they are invisible

--- src/app/navigators/drawerNavigator.js
+++ src/app/navigators/drawerNavigator.js
@@ -1,13 +1,43 @@
 import React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import HomeScreen from '../screens/HomeScreen';
+import HeaderStyle from '../screens/styles';
+import Icon from 'react-native-vector-icons/AntDesign'
 
 const Drawer = createDrawerNavigator();
 
 function DrawerNavigator() {
   return (
     <Drawer.Navigator>
-      <Drawer.Screen name="Ticket" component={HomeScreen} />
+      <Drawer.Screen
+        name="Home Screen"
+        component={HomeScreen}
+        options={{
+            title: 'Ticket',
+          headerStyle: {backgroundColor: '#04a744'},
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+            fontSize: 22
+          },
+          user: () => (
+            <Icon
+              name="user"
+              size={30}
+              color="#fff"
+              style={HeaderStyle.user}
+            />
+          ),
+          VerticalMenu: () => (
+            <Icon
+              name=""
+              size={30}
+              color="#fff"
+              style={HeaderStyle.verticalMenu}
+            />
+          ),
+        }}
+      />
     </Drawer.Navigator>
   );
 }
---
+++ src/app/screens/styles.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+
+const HeaderStyle = StyleSheet.create({
+    user: {
+        marginRight: 100
+    },
+    verticalMenu: {
+        marginRight: 90,
+    },
+})
+
+export default HeaderStyle;
