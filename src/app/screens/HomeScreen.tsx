1a0b8a329fec746f9559d9f4126cbde45ba68773
Aurnab Paul <aurnab06@gmail.com>
2024-06-02 16:24:04 +0600

    Home Screen is created

--- src/app/screens/HomeScreen.tsx
+++ src/app/screens/HomeScreen.tsx
@@ -1,10 +1,28 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React from 'react';
+import {Text, View} from 'react-native';
+import HomeStyle from './styles';
 
-function HomeScreen(){
-    return(
-        <Text>Home Screen</Text>
-    )
+export default function HomeScreen() {
+  return (
+    <View>
+      <View style={HomeStyle.chargeBoxContainer}>
+        <View style={HomeStyle.chargeBox}>
+          <Text style={HomeStyle.chargeBoxSaveText}>SAVE</Text>
+          <View style={HomeStyle.chargeBoxCharge}>
+            <Text style={HomeStyle.chargeBoxChargeText}>CHARGE</Text>
+            <Text style={HomeStyle.chargeBoxChargeText}>000.00</Text>
+          </View>
+        </View>
+      </View>
+
+      <View style={HomeStyle.foodItemsContainer}>
+        <View style={HomeStyle.foodItemsBox}>
+          <View style={HomeStyle.foodItemsBoxHeader}>
+            <Text style={HomeStyle.foodItemsBoxHeaderText}>COFFEE HUB</Text>
+          </View>
+        </View>
+      </View>
+    
+    </View>
+  );
 }
-
-export default HomeScreen;
--- src/app/screens/styles.js
+++ src/app/screens/styles.js
@@ -1,13 +1,53 @@
-import React from "react";
 import { StyleSheet } from "react-native";
 
-const HeaderStyle = StyleSheet.create({
-    user: {
-        marginRight: 100
+const HomeStyle = StyleSheet.create({
+    chargeBoxContainer: {
+        padding: 20,
+        flex: 1,
     },
-    verticalMenu: {
-        marginRight: 90,
+    chargeBox: {
+        height: 75,
+        width: 'auto',
+        backgroundColor: '#56bc07',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    chargeBoxSaveText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        alignSelf: 'center',
+        marginLeft: 60,
+        color: '#fff',
+    },
+    chargeBoxCharge: {
+        flexDirection: 'column',
+        justifyContent: 'center'
+    },
+    chargeBoxChargeText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        alignSelf: 'center',
+        marginRight: 60,
+        color: '#fff',
+    },
+    foodItemsContainer: {
+        marginVertical: 70,
+        flex: 1,
+    },
+    foodItemsBox: {
+        height: 100,
+        width: 350,
+        backgroundColor: '#b2babb'
+    },
+    foodItemsBoxHeader: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    foodItemsBoxHeaderText: {
+        fontSize: 18,
+        fontWeight: '400',
+        color: '#454545'
     },
 })
 
-export default HeaderStyle;
+export default HomeStyle;
